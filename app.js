f6c3b550c293c71f9a8f1885ac6e80550fe3334b
joemelt101 <joewebsites@outlook.com>
2017-08-23 21:15:25 -0500

    test

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = process.env.port||3000;
-connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log('Server running on 8080...');
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
+// var port = process.env.port||3000;
+// connect().use(serveStatic(__dirname)).listen(port, function(){
+//     console.log('Server running on 8080...');
+// });
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
 });
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
